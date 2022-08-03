dbf55fa14b53a4204019d00600fac1d98e79eebb
Patbox <39821509+patbox@users.noreply.github.com>
2022-08-03 08:18:00 -0400

    Allow loading of bigger icons (by scaling them down)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/io/github/haykam821/modviewer/MicroIconRenderer.java
+++ src/main/java/io/github/haykam821/modviewer/MicroIconRenderer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -18,9 +19,10 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.MathHelper;
 
 public final class MicroIconRenderer {
-	private static final int MAX_ICON_SIZE = 24;
+	private static final int MAX_ICON_SIZE = 32;
 
 	private static final String PIXEL_STRING = "█";
 	private static final String TRANSPARENCY_STRING = "▒";
@@ -49,16 +51,24 @@ public final class MicroIconRenderer {
 
 				Int2ObjectMap<Text> pixels = new Int2ObjectOpenHashMap<>();
 
-				MutableText[] rendered = new MutableText[image.getHeight()];
-				for (int y = 0; y < image.getHeight(); y += 1) {
-					rendered[y] = Text.empty();
-					for (int x = 0; x < image.getWidth(); x += 1) {
+				var stepSize = MathHelper.ceil(image.getWidth() / 32d);
+				var width = image.getWidth();
+				var height = image.getHeight();
+
+
+				var rendered = new ArrayList<MutableText>();
+
+
+				for (int y = 0; y < height; y += stepSize) {
+					var text = Text.empty();
+					for (int x = 0; x < width; x += stepSize) {
 						int argba = image.getRGB(x, y);
-						rendered[y].append(pixels.computeIfAbsent(argba, MicroIconRenderer::getPixel));
+						text.append(pixels.computeIfAbsent(argba, MicroIconRenderer::getPixel));
 					}
+					rendered.add(text);
 				}
 
-				return rendered;
+				return rendered.toArray(new MutableText[0]);
 			}
 		} catch (IOException exception) {
 			ModViewer.LOGGER.warn("Failed to render icon for mod {}", mod.getMetadata().getId(), exception);
@@ -69,12 +79,12 @@ public final class MicroIconRenderer {
 	private static boolean isInvalidSize(BufferedImage image) {
 		int width = image.getWidth();
 		if (width <= 0) return true;
-		if (width > MAX_ICON_SIZE) return true;
+		//if (width > MAX_ICON_SIZE) return true;
 
 		int height = image.getHeight();
 		if (height <= 0) return true;
-		if (height > MAX_ICON_SIZE) return true;
-		
+		//if (height > MAX_ICON_SIZE) return true;
+
 		return false;
 	}
 
